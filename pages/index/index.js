46251e5eec15e5e18cb07ecac84720e4a1a45f7c
wangld <wangld4195@163.com>
2019-05-10 11:58:36 +0800

    wangld

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,8 +11,8 @@ Page({
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+    wx.switchTab({
+      url: '../home/home'
     })
   },
   onLoad: function () {
