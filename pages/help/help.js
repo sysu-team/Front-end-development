a0692cf1ce3984da4598ba59f7ff8a4d1484e4ea
wangxm47 <1099422647@qq.com>
2019-06-27 02:33:04 +0800

    修复help页面bug

--- pages/help/help.js
+++ pages/help/help.js
@@ -6,16 +6,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    path: ""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var md = wx.getFileSystemManager().readFileSync('source/help.md','utf-8')
-    comi(md,this)
+  onLoad: function(options) {
+    var that = this
+    wx.getFileSystemManager().readdir({
+      dirPath: `${wx.env.USER_DATA_PATH}/help.md`,
+      success: res => {
+        var md = wx.getFileSystemManager().readFileSync(`${wx.env.USER_DATA_PATH}/help.md`, 'utf-8')
+        comi(md, that)
+      },
+      fail: res => {
+        console.log(res.errMsg)
+        wx.downloadFile({
+          url: 'https://raw.githubusercontent.com/sysu-team/Front-end-development/master/source/help.md',
+          success(res) {
+            if (res.statusCode === 200) {
+              console.log(res.tempFilePath)
+              wx.getFileSystemManager().saveFile({
+                tempFilePath: res.tempFilePath,
+                filePath: `${wx.env.USER_DATA_PATH}/help.md`,
+                success: res => {
+                  var md = wx.getFileSystemManager().readFileSync(`${wx.env.USER_DATA_PATH}/help.md`, 'utf-8')
+                  console.log(md)
+                  comi(md, that)
+                },
+                fail: res => {
+                  console.log(res.errMsg)
+                }
+              })
+            }
+          }
+        })
+      }
+    })
   },
 
-  
+
 })
